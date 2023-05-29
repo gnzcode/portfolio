f95fbf8f17140f37e5e8dbe44733a91c3bbb652d
gnzcode <gnzcode@gmail.com>
2023-05-28 18:58:54 -0600

    update 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: "/",
   env: {
     API_KEY: process.env.API_KEY,
     API_TOKEN: process.env.API_TOKEN,
